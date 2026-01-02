441405fa7d1951c57759ef173214e41869be922b
LJ-hyeok <junhyuk843@gmail.com>
2026-01-02 21:02:59 +0900

    add: Read

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ app.use(express.json());
 app.set('view engine','ejs');
 app.set("views", path.join(__dirname, "views"));
 
-let db = []; //임시 데이터베이스
+let db = [
+    {
+        title: "첫 번째 포스트",
+        text: "첫 포스트입니당 하하하"
+    },
+    {
+        title: "두 번째",
+        text: "우효효효"
+    },
+]; //임시 데이터베이스
 
 app.get("/", (req,res) => {
     res.render("index", {db});
@@ -22,9 +31,15 @@ app.get("/PostCreator", (req, res) => {
 app.post("/newFile", (req, res) => {
     const text = req.body.text;
     const title = req.body.title;
-    console.log(title, text);
     db.push({title: title, text: text});
     res.redirect("/");
+});
+
+app.get("/detail/:id", (req, res) => {
+    const title = req.params.id;
+    const index = db.findIndex((item) => item.title === title);
+    const text = db[index].text;
+    res.render("readFile", {title, text});
 });
 
 app.listen(8080, function(){
