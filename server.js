0ae81e32dbd9b4d00af945e51e613b6cb2784b5f
LJ-hyeok <junhyuk843@gmail.com>
2026-01-02 20:03:40 +0900

    add: read function

--- server.js
+++ server.js
@@ -3,24 +3,27 @@ const path = require('path');
 const app = express();
 // const cors = require("cors");
 
-app.use(express.urlencoded({ extended : true })); //textarea 해석하기 위해 필요
+app.use(express.urlencoded({ extended : true })); //from 해석하기 위해 필요
 // app.use(cors());
 app.use(express.json());
 app.set('view engine','ejs');
 app.set("views", path.join(__dirname, "views"));
 
+let db = []; //임시 데이터베이스
+
 app.get("/", (req,res) => {
-    res.render("index");
+    res.render("index", {db});
 });
 
-app.get("/PostCreate", (req, res) => {
+app.get("/PostCreator", (req, res) => {
     res.render("createFile");
 });
 
 app.post("/newFile", (req, res) => {
     const text = req.body.text;
-    console.log(text);
-    // res.render("index");
+    const title = req.body.title;
+    console.log(title, text);
+    db.push({title: title, text: text});
     res.redirect("/");
 });
 
